92fa9b6c174fc19f2af1ec233688389c1a466b5f
SerhiiKsenych <serhii.ksenych@gmail.com>
2018-02-12 05:42:25 +0200

    Cleaned

--- RegexTool/Regexer/IRegexHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.RegularExpressions;
-
-namespace Regexer
-{
-    public interface IRegexHelper
-    {
-        string RegexIt(string inputText, string pattern, RegexOptions options, bool isMultipleMatch);
-    }
-}
